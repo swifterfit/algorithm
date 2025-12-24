7f1fe194ca354da370537f7609963ac4767ca9c7
Emir <swifterfit@gmail.com>
2025-12-24 19:45:39 +0800

    fix 55

--- 55.swift
+++ 55.swift
@@ -3,7 +3,7 @@ class Solution {
         var maxReach = 0
         for i in 0..<nums.count {
             if i > maxReach { return false }  
-            maxReach = max(maxReach, i + num[i])
+            maxReach = max(maxReach, i + nums[i])
         }
         return true
     }
